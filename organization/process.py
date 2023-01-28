a91efa87294b9ee7cdd81f0faeadcc1e5964fbcc
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2023-01-28 10:45:39 +0100

    more precise output for meals

--- organization/process.py
+++ organization/process.py
@@ -10,11 +10,23 @@
 # 11 breakfast -> 16
 
 import csv
+import warnings
+
+# additional meals
+guests = [
+    ('Elise', 'Goujard', [5])
+    ]
 
 def date_index(entry, end=False):
     ans = entry.split(' ')
-    if len(ans) == 2:
+    date = shift = None
+
+    try:
         date = int(ans[0].strip())
+    except ValueError:
+        warnings.warn('unknown date {!r}'.format(ans[0]))
+
+    if len(ans) == 2:
         time = ans[1]
         if time == 'morning':
             shift = 0
@@ -23,23 +35,31 @@ def date_index(entry, end=False):
         elif time == 'evening':
             shift = 2
         else:
-            print('WARNING: what is time={!r}?'.format(time))
-            shift = 0 if date > 5 else 1
-        return 3 * (date - 5) + shift - 1
-    else:
-        return 17 if end else 0
+            warnings.warn('unknown time {!r}'.format(ans[1]))
+
+    if date is None:
+        date = 11 if end else 5
+
+    if shift is None:
+        if not end:
+            if date == 5:
+                shift = 1
+            else:
+                shift = 0
+        else:
+            if date == 11:
+                shift = 0
+            else:
+                shift = 2
+
+    i = 3 * (date - 5) + shift - 1
+    assert 0 <= i < 18, (entry, i)
+    return i
 
 def write_meals():
-    meal_numbers = [0] * 17
-    with open('nights_and_food.csv') as csvfile:
-        data = csv.DictReader(csvfile, delimiter=',')
-        for row in data:
-            arrival = row['arrival']
-            departure = row['departure']
-            for i in range(date_index(arrival), date_index(departure, end=True)):
-                meal_numbers[i] += 1
-
     with open('output/meals.csv', 'w') as meals:
+        meals.write('first name,')
+        meals.write('last name,')
         meals.write('dim 5 dîner,')
         meals.write('lun 6 petit déj, lun 6 midi, lun 6 soir,')
         meals.write('mar 7 petit déj, mar 7 midi, mar 7 soir,')
@@ -47,12 +67,43 @@ def write_meals():
         meals.write('jeu 8 petit déj, jeu 8 midi, jeu 8 soir,')
         meals.write('ven 9 petit déj, ven 9 midi, ven 9 soir,')
         meals.write('sam 10 petit déj\n')
-        meals.write(','.join(map(str, meal_numbers)))
 
-def write_mdn():
+        csv_rows = []
+        with open('nights_and_food.csv') as csvfile:
+            data = csv.DictReader(csvfile, delimiter=',')
+            for row in data:
+                start = date_index(row['arrival'])
+                end = date_index(row['departure'], end=True)
+                first_name = row['first name']
+                last_name = row['last name']
+
+                csv_row = [row['first name'], row['last name']]
+                csv_row += [int(start <= x < end) for x in range(17)]
+                csv_rows.append(csv_row)
+
+        for first_name, last_name, presence in guests:
+            csv_row = [first_name, last_name] + [0] * 17
+            for i in presence:
+                csv_row[2 + i] = 1
+            csv_rows.append(csv_row)
+
+
+        meal_numbers = [0] * 17
+        for csv_row in csv_rows:
+            meals.write(','.join(map(str, csv_row)))
+            meals.write('\n')
+            for i in range(17):
+                meal_numbers[i] += csv_row[i + 2]
+
+        meals.write('TOTAL,,')
+        meals.write(','.join(map(str, meal_numbers)))
+        meals.write('\n')
+
+
+def write_mdn_nights():
     with open('nights_and_food.csv') as csvfile:
         data = csv.DictReader(csvfile, delimiter=',')
-        with open('output/logement_mdn.csv', 'w') as mdn:
+        with open('output/nights_mdn.csv', 'w') as mdn:
             mdn.write('first name, last name, arrival, departure\n')
             for row in data:
                 first_name = row['first name']
@@ -64,4 +115,4 @@ def write_mdn():
                     mdn.write('{},{},{},{}\n'.format(first_name, last_name, arrival.split()[0], departure.split()[0]))
 
 write_meals()
-write_mdn()
+write_mdn_nights()
